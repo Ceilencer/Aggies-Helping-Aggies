d21494201d504e2db039cfd8b77bb8c37f01ef68
Coby Rafalik <cobyrafalik@gmail.com>
2026-04-13 13:43:25 -0500

    Fixing the 3 dots popup

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,6 +81,10 @@ module.exports = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
         card: {
           DEFAULT: "hsl(var(--card))",
           "header-text": "hsl(var(--card-header-text))",
